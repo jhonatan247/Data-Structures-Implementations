20ba7c8fa8ffe53917365e47776ef7337b0b7b20
jhonatan247 <yon.t.f@hotmail.com>
2018-08-27 22:02:57 -0500

    solution 1

--- Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio3;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Ejercicio3 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static long getNext(long a, long b, long c){
+        return a*b+c;
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        long n= s.nextInt(),a=s.nextInt(), b=s.nextInt(), c=s.nextInt(); 
+        
+        ArrayList<Long> l = new ArrayList();
+        for(long i =1; i<=n; i++){
+            l.add(i);
+        }
+        long initial =0;
+        long mod =0;
+        while(n>1){
+            mod = (initial+a)%n--;
+            l.remove(mod);
+            initial =mod;
+            if(initial>= n)
+                initial = 0;
+            a= a*b+c;
+        }
+        System.out.print(l.get(0));
+        
+        
     }
     
 }
