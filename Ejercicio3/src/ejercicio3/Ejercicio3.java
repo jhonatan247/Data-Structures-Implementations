3fce5bcbf6d6dd2f1adf92aee4d78ccdb5d925e4
jhonatan247 <30299033+jhonatan247@users.noreply.github.com>
2018-08-30 10:06:16 -0500

    Update Ejercicio3.java

--- Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -5,27 +5,24 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Ejercicio3 {
-
-    public static long getNext(long a, long b, long c){
-        return a*b+c;
-    }
+ 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        long n= s.nextInt(),a=s.nextInt(), b=s.nextInt(), c=s.nextInt(); 
-        
+        long n= s.nextInt();
+        long a=s.nextInt(), b=s.nextInt(), c=s.nextInt(); 
         ArrayList<Long> l = new ArrayList();
         for(long i =1; i<=n; i++){
             l.add(i);
         }
         long initial =0;
-        long mod =0;
+        int mod =0;
         while(n>1){
-            mod = (initial+a)%n--;
+            mod = (int)((initial+a)%n--);
             l.remove(mod);
             initial =mod;
             if(initial>= n)
                 initial = 0;
-            a= a*b+c;
+            a= (a*b+c)%(1000000007);
         }
         System.out.print(l.get(0));
         
